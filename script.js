bfde1bd967a4dcdd8b666ba57ac998352a6fed51
thor1105 <111889549+thor1105@users.noreply.github.com>
2022-08-23 20:29:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function heart(){
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left=Math.random()*100+"vw";
+    heart.style.animationDuration=Math.random()*2+3;
+    heart.innerText= "❤️";
+    document.body.appendChild(heart);
+    setTimeout(() =>{
+        heart.remove();
+    },3000)
+}
+setInterval(heart,200);
+
